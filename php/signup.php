00060991f589cf5203a31769b81002e04693956b
VivekJadaun <vivekjadaun08@gmail.com>
2017-06-06 17:29:04 +0530

    v1.2

--- php/signup.php
+++ php/signup.php
@@ -10,20 +10,26 @@
 	echo "Connected successfully.<br>";
 	$user = $_POST['user'];
 	$password = $_POST['pwd'];
-	$query = "SELECT * FROM userdata WHERE user_id='$user' LIMIT 1";
-	$result=mysqli_query($db,$query);
+	$retypepwd = $_POST['cpwd'];
+	if($password == $retypepwd){
+		$query = "SELECT * FROM userdata WHERE user_id='$user' LIMIT 1";
+		$result=mysqli_query($db,$query);
 
-	if (mysqli_num_rows($result) > 0){
-		echo "That username is taken. Please choose another one";
-	}
-	else{
-		$query = "INSERT INTO userdata (user_id,password) VALUES ('$user','$password')";
-		if(mysqli_query($db,$query)){
-			echo "Congratulations ".$user."! You are now registered with us";	
+		if (mysqli_num_rows($result) > 0){
+			echo "That username is taken. Please choose another one";
 		}
 		else{
-			echo "Sorry! Unable to insert into the database.<br>".mysqli_error($db);
+			$query = "INSERT INTO userdata (user_id,password) VALUES ('$user','$password')";
+			if(mysqli_query($db,$query)){
+				echo "Congratulations ".$user."! You are now registered with us";	
+			}
+			else{
+				echo "Sorry! Unable to insert into the database.<br>".mysqli_error($db);
+			}
 		}
+	}
+	else{
+		echo "Passwords don't match! Retype password";
 	}
 	mysqli_close($db);
 	?>
